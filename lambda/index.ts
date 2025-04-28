6e46a8c5a43e701a116e9e1b10f0d13bae1340bd
mikebalderasanchez <mikebalderassanchez@gmail.com>
2025-04-28 15:18:19 -0600

    feat: add better auth and postgresql

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,8 +1,11 @@
+import { auth } from "@/lib/auth";
 import { Hono } from "hono";
 import { handle } from "hono/aws-lambda";
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello Hono!"));
+
+app.on(["POST", "GET"], "/auth/**", (c) => auth.handler(c.req.raw));
 
 export const handler = handle(app);
