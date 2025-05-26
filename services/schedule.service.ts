cb1ded989b27e3e9d6d77c27f608abc9d032677b
mikebalderasanchez <mikebalderassanchez@gmail.com>
2025-05-25 23:37:53 -0600

    fix: schedule file names

---
+++ services/schedule.service.ts
@@ -0,0 +1,81 @@
+import { Shedule } from "@/lib/types";
+import {
+  scheduleRepository,
+  ScheduleRepository,
+} from "@/repositories/schedule.repository";
+
+export class SheduleService {
+  private sheduleRepository: ScheduleRepository;
+
+  constructor(sheduleRepository: ScheduleRepository) {
+    this.sheduleRepository = sheduleRepository;
+  }
+
+  public async getSchedule(user_id: string): Promise<Shedule[]> {
+    try {
+      const schedules = await this.sheduleRepository.getShedule(user_id);
+      return schedules;
+    } catch (error) {
+      console.error("Error al obtener los horarios:", error);
+      throw error;
+    }
+  }
+
+  public async createSchedule(
+    start_time: string,
+    end_time: string,
+    subjects_ids: string[],
+    user_id: string,
+  ): Promise<Shedule> {
+    const schedule = {
+      shedule_id: crypto.randomUUID(),
+      start_time,
+      end_time,
+      subjects_ids,
+      user_id,
+    };
+
+    try {
+      const createdSchedule =
+        await this.sheduleRepository.createSchedule(schedule);
+      return createdSchedule;
+    } catch (error) {
+      console.error("Error al crear el horario:", error);
+      throw error;
+    }
+  }
+
+  public async updateSchedule(
+    shedule_id: string,
+    start_time: string,
+    end_time: string,
+    subjects_ids: string[],
+    user_id: string,
+  ): Promise<void> {
+    const schedule = {
+      shedule_id,
+      start_time,
+      end_time,
+      subjects_ids,
+      user_id,
+    };
+
+    try {
+      await this.sheduleRepository.updateSchedule(schedule);
+    } catch (error) {
+      console.error("Error al actualizar el horario:", error);
+      throw error;
+    }
+  }
+
+  public async deleteSchedule(shedule_id: string): Promise<void> {
+    try {
+      await this.sheduleRepository.deleteSchedule(shedule_id);
+    } catch (error) {
+      console.error("Error al eliminar el horario:", error);
+      throw error;
+    }
+  }
+}
+
+export const sheduleService = new SheduleService(scheduleRepository);
---
+++ src/schedule.ts
@@ -0,0 +1,52 @@
+import { sheduleService } from "@/services/schedule.service";
+import { Hono } from "hono";
+
+export const shedule = new Hono();
+
+shedule.get("/", async (c) => {
+  const user_id = c.req.query("userId");
+
+  if (!user_id) {
+    return c.json({ error: "userId is required" }, 400);
+  }
+
+  const shedules = await sheduleService.getSchedule(user_id);
+
+  return c.json(shedules);
+});
+
+shedule.post("/", async (c) => {
+  const { start_time, end_time, subjects_ids, user_id } = await c.req.json();
+
+  const schedule = await sheduleService.createSchedule(
+    start_time,
+    end_time,
+    subjects_ids,
+    user_id,
+  );
+
+  return c.json(schedule);
+});
+
+shedule.put("/:shedule_id", async (c) => {
+  const { shedule_id } = c.req.param();
+  const { start_time, end_time, subjects_ids, user_id } = await c.req.json();
+
+  await sheduleService.updateSchedule(
+    shedule_id,
+    start_time,
+    end_time,
+    subjects_ids,
+    user_id,
+  );
+
+  return c.body(null, 204);
+});
+
+shedule.delete("/:shedule_id", async (c) => {
+  const { shedule_id } = c.req.param();
+
+  await sheduleService.deleteSchedule(shedule_id);
+
+  return c.body(null, 204);
+});
